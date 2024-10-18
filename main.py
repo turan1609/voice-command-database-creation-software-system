83415d2df77ac0a3a2b355bbc7e8a2e810b37b03
turan1609 <146887916+turan1609@users.noreply.github.com>
2024-10-19 01:16:40 +0300

    Database Created
    Database and voice table created.

--- main.py
+++ main.py
@@ -15,4 +15,23 @@ ui=Ui_MainWindow()
 ui.setupUi(penAna)
 penAna.show()
 
+#--------------- VERİTABANI OLUŞTURMA --------------
+#---------------------------------------------------
+
+import sqlite3
+global curs
+global conn
+conn=sqlite3.connect('veritabani.db')
+curs=conn.cursor()
+sorguCreTblSpor=("CREATE TABLE IF NOT EXISTS voice(                 \
+                 Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,   \
+                 Language TEXT NOT NULL,                       \
+                 Gender TEXT NOT NULL,                         \
+                 Name TEXT NOT NULL,                      \
+                 Commend TEXT NOT NULL,                          \
+                 Url TEXT NOT NULL UNIQUE,                             \
+                 Status BOOLEAN NOT NULL )")
+curs.execute(sorguCreTblSpor)# Bu yazdığımız sorguyu execute eder
+conn.commit()
+
 sys.exit(Uygulama.exec_())
